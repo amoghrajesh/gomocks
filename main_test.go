8804d23ab00771d852e4945435ac1318133972ac
Amogh <amoghrajesh1999@gmail.com>
2021-12-20 13:19:26 +0530

    Adding some extensive test cases

--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	mock_myinterface "gomocks/myinterface/mocks"
+	"math/rand"
 	"testing"
 )
 
@@ -14,7 +16,14 @@ func TestMyInterface(t *testing.T) {
 
 	m := mock_myinterface.NewMockMyInterface(ctrl)
 
-	m.EXPECT().IsUnder16(20).Return(false)
+	m.EXPECT().IsUnder16(gomock.Any()).MaxTimes(11)
+
+	for i := 0; i < 10; i++ {
+		r := rand.Int() % 20
+		fmt.Println("The random number is", r)
+		assert.Equal(t, m.IsUnder16(r), false)
+	}
+
 	assert.Equal(t, m.IsUnder16(20), false)
 
 }
